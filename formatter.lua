81f237abac4d4146a5500652f3a99a7543396d8b
koichirokudo <koichirokudo746@gmail.com>
2024-05-21 12:26:17 +0900

    add formatter.lua

---
+++ formatter.lua
@@ -0,0 +1,13 @@
+-- formatter.lua
+function format_text(lines)
+    for i, line in ipairs(lines) do
+        local formatted_line = line:gsub("([%a%d])([%z\128-\255])", "%1 %2")
+        formatted_line = formatted_line:gsub("([%z\128-\255])([%a%d])", "%1 %2")
+        lines[i] = formatted_line
+    end
+    vim.api.nvim_buf_set_lines(0, 0, -1, false, lines)
+end
+
+function echo()
+    print("Hello, world!")
+end
